3eccf7c4a0657bf66946678a93025f292c649ea9
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-10-06 10:07:18 -0300

    creation of app, server and db configuration

---
+++ source/server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import { productsRoutes } from './routes/index.js'
+import cors from 'cors'
+import { dbConnection } from './db/config.js'
+
+export class Server {
+  constructor () {
+    this.app = express()
+    this.middlewares()
+    this.routes()
+    this.connectionDb()
+  }
+
+  async connectionDb () {
+    await dbConnection()
+  }
+
+  middlewares () {
+    this.app.use(express.json())
+    this.app.use(cors())
+  }
+
+  routes () {
+    this.app.use('/products', productsRoutes)
+  }
+
+  listen () {
+    this.app.listen(8003, () => {
+      console.log('Servidor corriendo en el puerto 8003')
+    })
+  }
+}
