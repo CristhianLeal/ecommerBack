71ac75e13306d72641f9c85c7da0006fb4f1ff98
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-10-06 21:59:24 -0300

    routes update with login route

--- source/routes/products.routes.js
+++ source/routes/products.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { getProducts, getProduct, createProduct, deleteProduct, editProduct } from '../controllers/products.controllers.js'
+import { getProducts, getProduct, createProduct, deleteProduct, editProduct, log } from '../controllers/products.controllers.js'
 import { validateToken } from '../middlewares/validatesToken.js'
 const router = Router()
 
 router.get('/', getProducts)
+router.get('/log', log)
 router.get('/:id', getProduct)
 router.post('/', validateToken, createProduct)
 router.delete('/:id', validateToken, deleteProduct)
