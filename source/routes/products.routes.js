af791d65f4e1685ff20a03dd2b4324e4c25778f3
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-10-08 19:29:20 -0300

    products routes with validation of fields

--- source/routes/products.routes.js
+++ source/routes/products.routes.js
@@ -1,14 +1,34 @@
 import { Router } from 'express'
 import { getProducts, getProduct, createProduct, deleteProduct, editProduct, log, getByFilter } from '../controllers/products.controllers.js'
+import { body } from 'express-validator'
 import { validateToken } from '../middlewares/validatesToken.js'
+import { validateField } from '../middlewares/validatesFields.js'
 const router = Router()
 
 router.get('/', getProducts)
-router.post('/filter', getByFilter)
 router.get('/log', log)
 router.get('/:id', getProduct)
-router.post('/', validateToken, createProduct)
-router.delete('/:id', validateToken, deleteProduct)
-router.put('/:id', validateToken, editProduct)
+router.post('/filter', getByFilter)
+router.post('/',
+  [
+    body('name', 'El nombre es requerido y debe tener entre 3 y 20 caracteres').isLength({ min: 3, max: 20 }),
+    body('description', 'La descripcion es requerida y debe tener entre 5 y 30 caracteres').isLength({ min: 5, max: 30 }),
+    body('imageUrl', 'El nombre es requerido y debe tener entre 5 y 200 caracteres').isLength({ min: 5, max: 200 }),
+    body('price', 'El precio es requerido , debe ser un número y tener entre 2 y 10 numeros').isLength({ min: 2, max: 10 }).isNumeric(),
+    validateField
+  ],
+  validateToken,
+  createProduct)
 
+router.put('/:id',
+  [
+    body('name', 'El nombre es requerido y debe tener entre 3 y 20 caracteres').isLength({ min: 3, max: 20 }),
+    body('description', 'La descripcion es requerida y debe tener entre 5 y 30 caracteres').isLength({ min: 5, max: 30 }),
+    body('imageUrl', 'El nombre es requerido y debe tener entre 5 y 200 caracteres').isLength({ min: 5, max: 200 }),
+    body('price', 'El precio es requerido , debe ser un número y tener entre 2 y 10 numeros').isLength({ min: 2, max: 10 }).isNumeric(),
+    validateField
+  ],
+  validateToken,
+  editProduct)
+router.delete('/:id', validateToken, deleteProduct)
 export default router
