7ca35787120ace3084a00e5b4f0bb3adb14bdc92
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-10-07 21:17:44 -0300

    route update with filter

--- source/routes/products.routes.js
+++ source/routes/products.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { getProducts, getProduct, createProduct, deleteProduct, editProduct, log } from '../controllers/products.controllers.js'
+import { getProducts, getProduct, createProduct, deleteProduct, editProduct, log, getByFilter } from '../controllers/products.controllers.js'
 import { validateToken } from '../middlewares/validatesToken.js'
 const router = Router()
 
 router.get('/', getProducts)
+router.post('/filter', getByFilter)
 router.get('/log', log)
 router.get('/:id', getProduct)
 router.post('/', validateToken, createProduct)
