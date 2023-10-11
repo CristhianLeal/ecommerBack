2a9702329f1406169ae83f0c2028cf9c0fc2dbee
Cristhian Leal <cristhianaugustoleal@gmail.com>
2023-10-11 18:15:15 -0300

    validation for users

--- source/routes/users.routes.js
+++ source/routes/users.routes.js
@@ -1,8 +1,20 @@
 import { Router } from 'express'
+import { validateField } from '../middlewares/validatesFields.js'
+import { body } from 'express-validator'
 import { createUser, loginUser } from '.././controllers/users.controllers.js'
 const router = Router()
 
-router.post('/', createUser)
-router.post('/login', loginUser)
+router.post('/',
+  [
+    body('username', 'El username es requerido y debe tener entre 3 y 20 caracteres').isLength({ min: 3, max: 20 }),
+    body('password', 'La password es requerida y debe tener entre 5 y 30 caracteres').isLength({ min: 3, max: 20 }),
+    validateField
+  ], createUser)
+router.post('/login',
+  [
+    body('username', 'El username es requerido y debe tener entre 3 y 20 caracteres').isLength({ min: 3, max: 20 }),
+    body('password', 'La password es requerida y debe tener entre 5 y 30 caracteres').isLength({ min: 3, max: 20 }),
+    validateField
+  ], loginUser)
 
 export default router
